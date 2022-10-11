3089e4e11c62ed97140ca61ab040a0f0d82b5c31
wilson123104 <wilson123104@gmail.com>
2022-10-11 14:55:07 +0800

    pust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #導入 Discord.py
 import discord
-#from discord import app_commands 
+from discord import app_commands 
 import configload
 from datetime import datetime, timedelta
 
@@ -10,33 +10,28 @@ guild_id = configload.getConfigSetting("Credentials", "Guild_id")
 token = configload.getConfigSetting("Credentials", "Token")
 class aclient(discord.Client):
     def __init__(self):
-        super().__init__(intents = discord.Intents.default())
+        super().__init__(intents = discord.Intents.all())
         self.synced = False #we use this so the bot doesn't sync commands more than once
 
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced: #check if slash commands have been synced 
-            #await bot.sync(guild = discord.Object(id=guild_id)) #guild specific: leave blank if global (global registration can take 1-24 hours)
+            await bot.sync(guild = discord.Object(id=guild_id)) #guild specific: leave blank if global (global registration can take 1-24 hours)
             self.synced = True
         print(f'目前登入身份：{client.user}')
 
 client = aclient()
-#bot = app_commands.CommandTree(client)
+bot = app_commands.CommandTree(client)
 
 #當有訊息時
 @client.event
 async def on_message(message: discord.message.Message):
   global lastDate
-  if message.author == client:
+  if message.author == client.user:
     return
-  channel = client.get_channel(1027855117594152980)
-  await channel.send(
-            f'123'
-          )
   if message.author.id == target and datetime.today().strftime(
       "%Y%m%d") != lastDate:
     channel = client.get_channel(1022166337734324236)
-
     async for msg in channel.history(limit=10):
       if msg.author.id == 375805687529209857:
         time_1 = str(msg.created_at).split(".")[0]
@@ -55,11 +50,12 @@ async def on_message(message: discord.message.Message):
           await channel.send(
             f'<@{target}> 你已經有 {int(last_date)}天 {int(last_hour)}小時 {int(last_min)}分鐘 {int(last_seconds)}秒沒有直播了，我們都很想你，不如開個台吧!'
           )
+          return
         break
+  if message.content == "--v":
+    await message.channel.send("版本 1.0.0")
+    return
 
-
-#多選項
-"""
 @bot.command(guild = discord.Object(id=guild_id), name = 'ig', description='雨兒的Ig')
 async def slash2(interaction: discord.Interaction):
     await interaction.response.send_message("https://www.instagram.com/dear.yumii/") 
@@ -68,6 +64,8 @@ async def slash2(interaction: discord.Interaction):
 async def slash2(interaction: discord.Interaction):
     await interaction.response.send_message("https://www.twitch.tv/imuy_oxo") 
 
+#多選項
+"""
 @bot.command(guild = discord.Object(id=guild_id), name = 'tester2', description='testing')
 @app_commands.describe(fruits='fruits to choose from')
 @app_commands.choices(fruits=[
