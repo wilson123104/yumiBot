6c76d9e315cac32da7c99cb378aa83bca595d5a2
wilson123104 <wilson123104@gmail.com>
2022-10-13 17:33:11 +0800

    pust

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from discord import app_commands
 import configload
 from datetime import datetime, timedelta
 
-target = 303096379696349185
 lastDate = ''
 guild_id = configload.getConfigSetting("Credentials", "Guild_id")
 token = configload.getConfigSetting("Credentials", "Token")
+target = configload.getConfigSetting("Credentials", "TargetUser")
 class aclient(discord.Client):
     def __init__(self):
         super().__init__(intents = discord.Intents.all())
@@ -26,6 +26,7 @@ bot = app_commands.CommandTree(client)
 #當有訊息時
 @client.event
 async def on_message(message: discord.message.Message):
+  print(message.content)
   global lastDate
   if message.author == client.user:
     return
@@ -38,15 +39,15 @@ async def on_message(message: discord.message.Message):
         time_2 = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
         time_1_struct = datetime.strptime(
           time_1, "%Y-%m-%d %H:%M:%S") + timedelta(hours=8)
-        time_2_struct = datetime.strptime(time_2, "%Y-%m-%d %H:%M:%S")
+        time_2_struct = datetime.strptime(time_2, "%Y-%m-%d %H:%M:%S") + timedelta(hours=8)
         total_seconds = (time_2_struct - time_1_struct).total_seconds()
         last_date = total_seconds / 60 / 60 / 24
         last_hour = (last_date - int(last_date)) * 24
         last_min = (last_hour - int(last_hour)) * 60
         last_seconds = (last_min - int(last_min)) * 60
         channel = client.get_channel(message.channel.id)
-        lastDate = datetime.today().strftime("%Y%m%d")
-        if last_date > 2:
+        lastDate = datetime.today().strftime("%Y%m%d") + timedelta(hours=8)
+        if last_date > 3 and last_date % 2 == 0:
           await channel.send(
             f'<@{target}> 你已經有 {int(last_date)}天 {int(last_hour)}小時 {int(last_min)}分鐘 {int(last_seconds)}秒沒有直播了，我們都很想你，不如開個台吧!'
           )
@@ -64,6 +65,19 @@ async def slash2(interaction: discord.Interaction):
 async def slash2(interaction: discord.Interaction):
     await interaction.response.send_message("https://www.twitch.tv/imuy_oxo") 
 
+@bot.command(guild = discord.Object(id=guild_id), name = 'test', description='雨兒的Twitch')
+async def slash2(interaction: discord.Interaction):
+    #await interaction.guild.get_member(511177977015435305).edit()
+    #await interaction.guild.get_member(511177977015435305).move_to(interaction.guild.get_channel(1022529624661557302))
+    #print(interaction.guild.get_member(511177977015435305).voice)
+    await interaction.response.send_message("<:imuyoxCry:1028753645325537290>") 
+    #台主好棒好漂亮 imuyoxHeart
+@bot.error
+async def on_app_command_error(interaction: discord.Interaction, error: discord.app_commands.AppCommandError) -> None:
+  print(error)
+  #await interaction.response.send_message(error) 
+
+  
 #多選項
 """
 @bot.command(guild = discord.Object(id=guild_id), name = 'tester2', description='testing')
