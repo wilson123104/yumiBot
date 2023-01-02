a639a2fa0bc54f78037fb1d4f8e4732e1d22e761
wilson123104 <wilson123104@gmail.com>
2023-01-03 00:58:01 +0800

    push

--- main.py
+++ main.py
@@ -85,14 +85,19 @@ bot.add_command(posterBoard.posterBoardCommandGroup(name='貼堂文'),guild= dis
 async def test(interaction: discord.Interaction,user:discord.Member):
     selectUser = dbConn.selectUserById(user.id)
     channel = client.get_channel(int(textChannel))
+    name = ""
+    if user.nick == None:
+        name = user.name
+    else:
+        name = user.nick
     if interaction.channel_id == int(textChannel):
         if len(selectUser) != 0:
             if selectUser[0][4] != 0:
-                await interaction.response.send_message(f"從2023年1月3日到現在已經講過{selectUser[0][4]}句話。")
+                await interaction.response.send_message(f"{name}從2023年1月3日到現在已經講過{selectUser[0][4]}句話。")
             else:
-                await interaction.response.send_message("從2023年1月3日到現在沒有講過話。")
+                await interaction.response.send_message(f"{name}從2023年1月3日到現在沒有講過話。")
         else:
-            await interaction.response.send_message("從2023年1月3日到現在沒有講過話。")
+            await interaction.response.send_message(f"{name}從2023年1月3日到現在沒有講過話。")
             
     else:
         await interaction.response.send_message(f"請在{channel.name}頻道使用這指令",ephemeral=True)
